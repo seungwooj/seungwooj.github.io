9cb9a6fecc5f2b65809e9f5d8093d8816c348a26
seungwooj <seungwoo.aaron@gmail.com>
2022-04-19 04:20:19 +0900

    comment out prefix

--- src/next.config.js
+++ src/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
 	reactStrictMode: true,
-	assetPrefix: isProd
-		? "https://cdn.statically.io/gh/seungwooj/seungwooj.github.io/"
-		: "",
+	// assetPrefix: isProd
+	// 	? "https://cdn.statically.io/gh/seungwooj/seungwooj.github.io/"
+	// 	: "",
 	images: {
 		loader: "akamai",
 		path: "",
