e916d2e1f0b40276f51a1075a92e315c072359c6
seungwooj <seungwoo.aaron@gmail.com>
2022-04-19 03:47:32 +0900

    Add CDN

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+https://cdn.statically.io/gh/:user/:repo/:tag/:file
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/seungwooj/seungwooj.github.io/' : '',
 }
 
 module.exports = nextConfig
