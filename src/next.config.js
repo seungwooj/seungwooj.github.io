57c88826bb77228236b633ef693be298dcb58051
seungwooj <seungwoo.aaron@gmail.com>
2022-04-19 03:49:47 +0900

    Add CDN-fix error

--- src/next.config.js
+++ src/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-https://cdn.statically.io/gh/:user/:repo/:tag/:file
-
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/seungwooj/seungwooj.github.io/' : '',
-}
+	reactStrictMode: true,
+	assetPrefix: isProd
+		? "https://cdn.statically.io/gh/seungwooj/seungwooj.github.io/"
+		: "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
