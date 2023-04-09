d38d78474d356856ac0baf527821e78d10aaa672
seungwooj <seungwoo.aaron@gmail.com>
2023-04-09 04:29:54 +0000

    Deploying to gh-pages from @ seungwooj/seungwooj.github.io@f17d4b4f07e4674d153b8ab94075aff553bf5ede 🚀

---
+++ _next/static/9FKTwY6DdbQh8cs4kBASY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-c021b221b5dcdb80.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
